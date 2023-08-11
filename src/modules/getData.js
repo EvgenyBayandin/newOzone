110eb5a7630771bb9bf43a9474ab775d068f53f1
EvgenyBAyandin <5087081@gmail.com>
2023-08-11 22:51:39 +0300

    update project, added simple-notify function, added trow catch error notify

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,10 +1,26 @@
+import Notify from 'simple-notify'
+
 const getData = (str) => {
 
 	return fetch('https://test-af218-default-rtdb.firebaseio.com/goods.json')
-	.then ((response) => {
-		return response.json()
-	})
-	
+		.then((response) => {
+		
+			if(response.ok){
+				return response.json()
+			} else {
+			throw new Error('Ошибка выполнения запроса к БД!')
+			}
+		})
+		.catch(error => {
+				new Notify({
+				status: 'error',
+				title: 'Ошибка!',
+				text: error.message,
+				effect: 'slide',
+				type: 3,
+				autoclose: true
+			})
+		})
 }
 
 export default getData
